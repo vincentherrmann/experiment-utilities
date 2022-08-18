26bd3f3d1190067c9d4418b494c69b17a5c113bf
vincentherrmann <vincent.herrmann@web.de>
2022-08-18 11:43:42 +0200

    add tree functionalities

--- experiment_utilities/tests/test_trees.py
+++ experiment_utilities/tests/test_trees.py
@@ -1,16 +1,37 @@
-from experiment_utilities.trees import tree_flatten, tree_fill
+from experiment_utilities.trees import tree_flatten, tree_fill, tree_map
+
+tree1 = [[[3, 5, 6],
+          [1, 7]],
+         [4, 5],
+         8]
+
+tree2 = [
+    {"foo": (4,
+             [2, 3]),
+     "bar": [5, {1: 34,
+                 's': 6}]
+     },
+    [2, 7, (8, 4)]
+]
 
 
 def test_tree_flatten():
-    tree1 = [
-        {"foo": (4,
-                 [2, 3]),
-         "bar": [5, {1: 34,
-                     's': 6}]
-         },
-        [2, 7, (8, 4)]
-    ]
-    tree2 = [[[3, 5, 6], [1, 7]], [4, 5], 8]
+    leaves, structure = tree_flatten(tree1)
+    filled_tree = tree_fill(leaves, structure)
+
+    assert filled_tree == tree1
+
     leaves, structure = tree_flatten(tree2)
     filled_tree = tree_fill(leaves, structure)
     assert filled_tree == tree2
+
+
+def test_tree_map():
+    processed_tree = tree_map(f=lambda x: x+1, tree=tree2)
+    assert processed_tree[0]["foo"][0] == 5
+    assert processed_tree[1][1] == 8
+
+    added_tree = tree_map(f=lambda x, y, z: x + y - z, tree=tree2, rest=(processed_tree, tree2))
+    assert added_tree[0]["foo"][0] == 5
+    assert added_tree[1][1] == 8
+    pass
--- experiment_utilities/trees.py
+++ experiment_utilities/trees.py
@@ -1,42 +1,54 @@
-TREE_CONTAINER_TYPES = [list, tuple, dict]
-
-
 def tree_flatten(tree, is_leaf=None):
-    def traverse_leaves(node):
+    def traverse_leaves(node, is_leaf=None):
         leaves = []
-        if type(node) in TREE_CONTAINER_TYPES:
+        if is_leaf is not None and is_leaf(node):
+            return [node]
+        elif type(node) is list or type(node) is tuple:
             for child in node:
-                l = traverse_leaves(child)
-                leaves.extend(l)
+                leaves.extend(traverse_leaves(child, is_leaf))
+        elif type(node) is dict:
+            for key, child in node.items():
+                leaves.extend(traverse_leaves(child, is_leaf))
         else:
             return [node]
         return leaves
 
-    def traverse_structure(node):
-        if type(node) in TREE_CONTAINER_TYPES:
-            nested_list = [traverse_structure(child) for child in node]
+    def traverse_structure(node, is_leaf=None):
+        if is_leaf is not None and is_leaf(node):
+            return None
+        elif type(node) is list:
+            structure = [traverse_structure(child, is_leaf) for child in node]
+        elif type(node) is tuple:
+            structure = tuple([traverse_structure(child, is_leaf) for child in node])
+        elif type(node) is dict:
+            structure = {key: traverse_structure(child, is_leaf) for key, child in node.items()}
         else:
             return None
-        return nested_list
+        return structure
 
-    # def traverse(node, structure=None):
-
-        # if type(node) in TREE_CONTAINER_TYPES:
-        #     for child in node:
-        #         traverse(child)
-        # else:
-        #     print(node)
-        #     return None
-
-    leaves = traverse_leaves(tree)
-    structure = traverse_structure(tree)
+    leaves = traverse_leaves(tree, is_leaf)
+    structure = traverse_structure(tree, is_leaf)
     return leaves, structure
 
 
-def tree_fill(leaves, structure):
-    if type(structure) in TREE_CONTAINER_TYPES:
-        filled_structure = [tree_fill(leaves, child) for child in structure]
+def tree_fill(leaves, structure, is_leaf=None):
+    if is_leaf is not None and is_leaf(structure):
+        return leaves.pop(0)
+    if type(structure) is list:
+        filled_structure = [tree_fill(leaves, child, is_leaf) for child in structure]
+    elif type(structure) is tuple:
+        filled_structure = tuple([tree_fill(leaves, child, is_leaf) for child in structure])
+    elif type(structure) is dict:
+        filled_structure = {key: tree_fill(leaves, child, is_leaf) for key, child in structure.items()}
     else:
         return leaves.pop(0)
     return filled_structure
+
+
+def tree_map(f, tree, rest=[], is_leaf=None):
+    leaves, structure = tree_flatten(tree, is_leaf)
+    all_leaves = [leaves] + [tree_flatten(r, is_leaf)[0] for r in rest]
+    processed_leaves = [f(*xs) for xs in zip(*all_leaves)]
+    processed_tree = tree_fill(processed_leaves, structure, is_leaf)
+    return processed_tree
 

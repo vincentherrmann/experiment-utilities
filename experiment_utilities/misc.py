3c66a7937fd8f3bb7470bcd151d0b7f68e15932a
vincentherrmann <vincent.herrmann@web.de>
2023-04-25 11:45:40 +0200

    add num_params function

--- experiment_utilities/misc.py
+++ experiment_utilities/misc.py
@@ -1,5 +1,24 @@
+import torch
+
 # Turns a dictionary into a class
 class Dict2Class(object):
     def __init__(self, d):
         for key in d:
             setattr(self, key, d[key])
+
+
+def num_params(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+
+hash_key_1 = torch.tensor(0x58a849af6cbf585a, dtype=torch.long)
+hash_key_2 = torch.tensor(0x5ca118cb4d828a5e, dtype=torch.long)
+def hash64(x):
+    # Convert the input to a 64-bit unsigned integer tensor
+    x = torch.tensor(x, dtype=torch.long)
+    # Use the PyTorch's bitwise XOR operator to generate a hash value
+    # We use two random 64-bit integers as the hash key
+    # Note: the key can be any fixed 64-bit integer or a set of integers
+    h = torch.bitwise_xor(x ^ hash_key_1.to(x.device), hash_key_2.to(x.device))
+    return h
